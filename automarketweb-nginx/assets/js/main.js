3fee53a80b5daec715f063a7fc20b09c303e0b82
David Franco <franco.cpersonal@gmail.com>
2025-04-26 13:48:30 -0500

    refactor: Eliminar microservicio Vehiculos original y añadir KafkaeventsMS

--- automarketweb-nginx/assets/js/main.js
+++
@@ -1,4 +0,0 @@
-// Código JS personalizado (puedes agregar validaciones, manejo de modales, etc.)
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('AutoMarket Frontend listo.');
-});
